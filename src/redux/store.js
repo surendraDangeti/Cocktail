f618814aa4d900817d53a5458c754eaab13cffde
surendraDangeti <89248419+surendraDangeti@users.noreply.github.com>
2021-12-13 10:17:49 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,9 +6,9 @@ import rootReducer from './root-reducer';
 
 const middleware = [thunk];
 
-if(process.env.NODE_ENV !== 'production'){
-    middleware.push(logger);
-}
+// if(process.env.NODE_ENV !== 'production'){
+//     middleware.push(logger);
+// }
 
 const store = createStore(rootReducer, applyMiddleware(...middleware));
 
