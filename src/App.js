d68ea27044fc082c06324044a4e5d335e809b6f4
nookesh <nookeshkarri7@gmail.com>
2021-12-14 09:32:04 +0530

    changed app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import Home from './pages';
-import {   Routes ,Route } from "react-router-dom"
-import Header from './components/Header'
-import SingleCocktail from './pages/SingleCocktail'
-import './App.css';
+import Home from "./pages";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import SingleCocktail from "./pages/SingleCocktail";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-       <Header/>
-       <Routes>
-        <Route exact path = "/" element = {<Home/>}/> 
-        <Route exact path = "/cocktail/:id" element = {<SingleCocktail/>}/> 
-       </Routes>
+      <Header />
+      <h1>Hi this is Nookesh </h1>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/cocktail/:id" element={<SingleCocktail />} />
+      </Routes>
     </div>
   );
 }
